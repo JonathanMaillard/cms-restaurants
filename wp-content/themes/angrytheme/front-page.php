3a0456274f25f309c2692eee68a78b9e189721f6
JonathanMaillard <jonathan.maillard95@gmail.com>
2021-01-11 16:21:33 +0100

    first section front page

--- wp-content/themes/angrytheme/front-page.php
+++ wp-content/themes/angrytheme/front-page.php
@@ -1,5 +1,43 @@
 <?php get_header(); ?>
 
-<h1>Angry test</h1>
+<section class="qualities">
+
+    <div class="qualities__quality">
+
+        <div class="qualities__image">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/svg/quality-food.svg" />
+        <div>
+
+        <div class="qualities__text">
+            QUALITY FOODS
+        </div>
+
+    </div>
+
+    <div class="qualities__quality">
+
+        <div class="qualities__image">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/svg/fastest-delivery.svg" />
+        <div>
+        
+        <div class="qualities__text">
+            FASTEST DELIVERY
+        </div>
+
+    </div>
+
+    <div class="qualities__quality">
+
+        <div class="qualities__image">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/svg/original-recipes.svg" />
+        <div>
+        
+        <div class="qualities__text">
+            ORIGINAL RECIPES
+        </div>
+
+    </div>
+
+</section>
 
 <?php get_footer(); ?>
