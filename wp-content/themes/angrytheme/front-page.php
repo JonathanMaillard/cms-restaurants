6d4f66aec145e3a63e7058e5aced8721d8f052a4
JonathanMaillard <jonathan.maillard95@gmail.com>
2021-01-11 17:00:30 +0100

    front page second section image and title

--- wp-content/themes/angrytheme/front-page.php
+++ wp-content/themes/angrytheme/front-page.php
@@ -41,7 +41,16 @@
 </section>
 
 <section class="story">
-    
+
+    <div class="story__image">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/the-chef.jpg">
+    </div>
+
+    <div class="story">
+        <h2>Discover<br>OUR STORY</h2>
+        <h4></h4>
+    </div>
+
 </section>
 
 <?php get_footer(); ?>
