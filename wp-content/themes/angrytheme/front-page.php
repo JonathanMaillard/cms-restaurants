6ff569722d494dc8072e148e9d02047b97db09b6
JonathanMaillard <jonathan.maillard95@gmail.com>
2021-01-11 10:48:55 +0100

    Initial commit

---
+++ wp-content/themes/angrytheme/front-page.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+<h1>Angry test</h1>
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/angrytheme/functions.php
@@ -0,0 +1,10 @@
+<?php
+
+//----------- Deactivate Gutenberg editor ---------
+
+// for posts
+add_filter('use_block_editor_for_post', '__return_false', 10);
+// for post types
+add_filter('use_block_editor_for_post_type', '__return_false', 10);
+
+// -------------------------------------------------
