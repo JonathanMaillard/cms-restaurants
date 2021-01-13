9fc3a5e40b963d1c8d4bb7e20ab2a9bb5103bef6
SandrineLe <le.sandrinemt@gmail.com>
2021-01-13 10:02:21 +0100

    header + recipes

--- wp-content/themes/angrytheme/footer.php
+++ wp-content/themes/angrytheme/footer.php
@@ -1,4 +1,9 @@
-<footer><p>bob le footer</p></footer>
+
+
+
+<footer>
+<img src="<?php echo get_template_directory_uri(); ?>/assets/images/hachures-noires.png" alt="linebreaker" class="black-hatching">
+<p>bob le footer</p></footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     <?php wp_footer(); ?>
 </body>
