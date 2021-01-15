05e7f5f3701bf007c882488ddb129e5e00d8207f
SandrineLe <le.sandrinemt@gmail.com>
2021-01-15 11:32:39 +0100

    footer

--- wp-content/themes/angrytheme/footer.php
+++ wp-content/themes/angrytheme/footer.php
@@ -1,9 +1,38 @@
 
 
+<footer>
 
-<!--<footer>
-<img src="<?php echo get_template_directory_uri(); ?>/assets/images/hachures-noires.png" alt="linebreaker" class="black-hatching">
-<p>bob le footer</p></footer>-->
+<div class="container">
+  <div class="row pt-5">
+    <div class="col-sm">
+      <p class="h3">Dev Restaurant</p>
+      <p><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat nunc sed dolor dictum, vitae lacinia odio volutpat. Integer at ante pellentesque, viverra nulla luctus, pellentesque ex.</span> </p>
+      <div class="socialmed pt-3">
+      <a href="#"><i class="fab fa-facebook-f socialmed__fb"></i></a>
+      <a href="#"><i class="fab fa-twitter socialmed__twitter"></i></a>
+      <a href="#"><i class="fab fa-instagram socialmed__ig"></i></a>
+      <a href="#"><i class="fab fa-linkedin-in socialmed__linkedin"></i></a>
+      </div>
+    </div>
+    <div class="col-sm">
+    <p class="h6">Open hours</p>
+    <div class="grid-container">
+  <div class="time"><i class="far fa-clock"></i></div>
+  <div class="day"><p>Monday</p></div>
+  <div class="line"><hr></div>
+  <div class="hours"><p>Closed</p></div>
+</div>
+    </div>
+    <div class="col-sm">
+    <p class="h6">Contact us</p>
+    </div>
+    <div class="col-sm">
+    <p class="h6">Instagram</p>
+    </div>
+  </div>
+</div>
+
+</footer>
 <script src="script.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
@@ -11,3 +40,4 @@
     <?php wp_footer(); ?>
 </body>
 </html>
+
