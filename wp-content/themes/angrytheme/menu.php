b41e6acbcba0e845fb02e124b1936d7711bb698b
SandrineLe <le.sandrinemt@gmail.com>
2021-01-20 11:36:03 +0100

    menu.php

--- wp-content/themes/angrytheme/menu.php
+++ wp-content/themes/angrytheme/menu.php
@@ -4,3 +4,29 @@
     Template Post Type: page
     */
 ?>
+<?php get_header(); ?>
+
+<body style="background-color:#F5F5F5;">
+
+<div class="headerpicture">
+<img src="<?php echo get_template_directory_uri(); ?>/assets/images/menu-lg.jpg" class="black" alt="header">
+</div>
+
+<div class="block px-5 py-3">
+<p class="block__maintitle">Let's find out
+<span>The menu</span></p>
+<div class="block__mainlink">
+<p><a class="liner text-white ml-5" href="#">Order online </a></p>
+</div>   
+</div>
+
+<div class="grey-hatching"></div>
+
+<section class="menu">
+       <div class="menu__title">
+       <p>Welcome <span>The Menu</span></p>
+       
+
+</section>
+
+<?php get_footer(); ?>
