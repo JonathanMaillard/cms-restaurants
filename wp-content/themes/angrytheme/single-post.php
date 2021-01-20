7609b8add6864eb858b4e0d65a4863f9f8d74a1e
Magali Garot <magali.garot@gmail.com>
2021-01-20 14:08:22 +0100

    articlemiseneforme

--- wp-content/themes/angrytheme/single-post.php
+++ wp-content/themes/angrytheme/single-post.php
@@ -4,12 +4,12 @@
 <div class="containerArticle">
     <?php if( have_posts() ): while( have_posts()) : the_post(); ?>
     <!-- " : " signifie s'il y a alors -->
-    <div class="article">
+  
     <!-- TITLE ARTICLE -->
         <h1 class="article__Title"><?php the_title() ?></h1>
     
         <?php endwhile; endif; ?>  
-    </div>
+ 
 
     <!-- ACF CHAMPS -->
     <?php if( has_category( 'Recipes' ) ): ?>
@@ -18,10 +18,14 @@
         <div class="review">
             <!-- SUBTILE ARTICLE -->
             <h3 class="article__subtile"><?php the_field('subtitle'); ?></h3>
+        </div>    
 
              <!-- PICTURE ARTICLE -->
             <img class="article__Picture" src="<?php echo get_field('image_article')['url']; ?>">
 
+  
+
+    <div class="flexDesktop">     
             <!-- LINK ARTICLE -->
             <div class="link">
                 <a class="facebook" href="<?php the_field('facebook'); ?>">
@@ -46,9 +50,10 @@
             </div>
             <!-- INGREDIENT ARTICLE -->
             <div class="ingredient">
-            <h4 class="ingredient__title"><?php the_field('title_ingredient'); ?></h4>
-                <p class="ingredients__text"><?php echo '<br/>'. get_field('ingredients') . '<br/>'; ?></p>
+                <h4 class="ingredient__title"><?php the_field('title_ingredient'); ?></h4>
+                <p class="ingredient__text"><?php echo "<br/>". get_field("ingredients") . "<br/>"; ?></p>
             </div>
+    </div>   
 
               <!-- INSTRUCTIONS ARTICLE -->
               <div class="instructions">
@@ -64,7 +69,6 @@
                         echo '<li class="instructions__liste">' . $value['instruction'] . '</li>';
                         // var_dump($value)["picture"];
                         echo '<img class="instructions__picture" src="'. $value["picture"]["url"] . '">';
-                        
                         // rajouter des corchets avec ce qu'il faut mettre entre crochet
                     }
                     
@@ -75,6 +79,7 @@
             <?php endif; ?>
         </div>
 
+        <!-- ICON SMARPHONE TABLETTE -->
         <div class="linkSmart">
                 <a class="facebook" href="<?php the_field('facebook'); ?>">
                     <i class="fab fa-facebook-square iconarticle fb">
@@ -98,7 +103,7 @@
             </div>
 </div>
 
-<?php
+<?php get_footer(); ?>
 
           
 
