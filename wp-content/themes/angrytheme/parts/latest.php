6a6c667b813f23762cb2792806107c37557dacaf
Magali Garot <magali.garot@gmail.com>
2021-01-22 10:26:24 +0100

    latest

--- wp-content/themes/angrytheme/parts/latest.php
+++ wp-content/themes/angrytheme/parts/latest.php
@@ -31,12 +31,19 @@ if ( $the_query->have_posts() ) {
         echo '<div class="container p-5" style="width:75%;">';
         echo '<div class="row g-0">';
         echo '<div class="col">';
+        // PICTURE
         echo '<div class="card border-0 entry" style="width:19rem;">';
         echo '<div class="card" style="width: 20rem;">' . the_post_thumbnail('medium', ['class' => 'card-img-top', 'alt' => '', 'style'=> 'height:auto;']) . '</div>';
-        echo '<p class="entry__time">' . '<i class="far fa-clock">' . '</i>' . the_time( get_option( 'date_format' ) ) . '</p>';
+        echo '<p class="entry__time">';
+        echo '<i class="far fa-clock">' . '</i>';
+        echo the_time( get_option( 'date_format' ) );
+        echo '</p>';
         echo '<p class="h5">' . get_the_title() . '</p>';
-        echo '<p class="h5">' . get_field('subtitle') . '</p>';
-        echo '<div class="horizontal_dotted_line">' . '<a href="' . get_permalink() . '">' . 'Read More' . '</a>' . '</div>';
+        echo '<p class="subtileCard">' . get_field('subtitle') . '</p>';
+        echo '<div class="horizontal_dotted_line">';
+        echo '<span class="dot">' . '</span>';
+        echo '<a class="readMore" href="' . get_permalink() . '">' . 'Read More' . '</a>';
+        echo '</div>';
         echo '</div>';
         echo '</div>';
         echo '</div>';
